39adac96d7746110d8fd582371aebe4b1bd08dbe
Watts-Blake <watts.blake52@gmail.com>
2023-09-12 01:13:03 -0700

    exercise a complete (actually complete now -_-

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,5 +1,10 @@
+const STARTING_MISSILES : i32 = 8;
+const READY_AMOUNT : i32 = 2;
+
+
 fn main() {
-    let missiles : i32 = 8;
-    let ready : i32 = 2;
-    println!("Firing {} of my {} missiles...", ready, missiles)
+    let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
 }
